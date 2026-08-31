42ea93dd001c0133bd08d4332f9913e0088cd186
majidmahmoodlhr <57787986+majidmahmoodlhr@users.noreply.github.com>
2026-08-31 16:47:49 +0500

    Update app.py

--- app.py
+++ app.py
@@ -693,14 +693,5 @@ def cut_video():
 # START SERVER
 # =========================================================
 
-if __name__ == "__main__":
-
-    app.run(
-
-        host="127.0.0.1",
-
-        port=5000,
-
-        debug=True
-
-    )
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
